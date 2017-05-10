166fa7dcb395f67143f7fc446e8e04dcdfe5c742
Doola <ahmed.adel.abodeif@gmail.com>
2017-05-10 23:20:20 +0200

    first commit

---
+++ InstaNetFrame/InstaNetFrame.h
@@ -0,0 +1,19 @@
+//
+//  InstaNetFrame.h
+//  InstaNetFrame
+//
+//  Created by Ahmed Abodeif on 5/10/17.
+//  Copyright © 2017 Ahmed Abodeif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstaNetFrame.
+FOUNDATION_EXPORT double InstaNetFrameVersionNumber;
+
+//! Project version string for InstaNetFrame.
+FOUNDATION_EXPORT const unsigned char InstaNetFrameVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaNetFrame/PublicHeader.h>
+
+
---
+++ InstaNetFrameTests/InstaNetFrameTests.m
@@ -0,0 +1,39 @@
+//
+//  InstaNetFrameTests.m
+//  InstaNetFrameTests
+//
+//  Created by Ahmed Abodeif on 5/10/17.
+//  Copyright © 2017 Ahmed Abodeif. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface InstaNetFrameTests : XCTestCase
+
+@end
+
+@implementation InstaNetFrameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
